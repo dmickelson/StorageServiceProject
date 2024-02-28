066b8f78362239fe3c9f3f12dfcccb05b65bd05b
dmickelson <davidpmickelson@gmail.com>
2024-02-28 16:03:46 -0500

    updates

--- storageservice/awsmysqllib.py
+++ storageservice/awsmysqllib.py
@@ -1,42 +1,144 @@
-import configparser
 import pymysql
-
-host = 'database-1.clkmgeoe0nsn.us-east-1.rds.amazonaws.com'
-user = 'admin'
-password = 'r18&GL#i'
-database = 'database-1'
-port = 3306
-
-# zoho_auth.init_from_file('zohoauth.cfg')
-
-try:
-    connection = pymysql.connect(
-        host=host, user=user, password=password, database=database, port=port)
-    with connection:
-        cur = connection.cursor()
-        cur.execute("SELECT VERSION()")
-        version = cur.fetchone()
-        print("Database version: {} ".format(version[0]))
-    connection.close()
-except Exception as e:
-    print(f"Error: {e}")
+import configparser
+import socket
+import logging
+import logging.config
+logging.config.fileConfig('logging_storageservice.cfg')
 
 
-# def init_from_file(self, file_name: str) -> None:
-#        self.logger.info("Loading configuration from file: "+file_name)
-#        # * read token in from a Config formated file. Requires the ZOHO_AUTH section
-#        config = configparser.ConfigParser()
-#        config.read(file_name)
-#        try:
-#             details = config['ZOHO_AUTH']
-    #     self._client_id = details['client_id']
-    #     self._client_secret = details['client_secret']
-    #     self._redirect_url = details['redirect_url']
-    #     self._refresh_token = details['refresh_token']
-    #     self._access_token = details['access_token']
-    #     self._expires_in = datetime.datetime.strptime(
-    #         details['expires_in'], '%Y-%m-%d %H:%M:%S')
-    # except configparser.NoOptionError as err:
-    #     self.logger.exception("configparser.NoOptionError:")
-    #     self.logger.exception(err)
-    # return cls(details['client_id'], details['client_secret'], details['redirect_url'], details['refresh_token'], details['access_token'])
+class AWSMySQLLib:
+    def __init__(self, host, user, password, database, port):
+        self.host = host
+        self.user = user
+        self.password = password
+        self.database = database
+        self.port = port
+        self.connection = None
+        self.logger = logging.getLogger(self.__class__.__name__)
+        self.logger.info(
+            f"Creating an instance of {str(self.__class__.__name__)}")
+        self.logger.info("Initializing AWSMySQLLib")
+
+    @classmethod
+    def init_from_file(cls, file_name: str):
+        config = configparser.ConfigParser()
+        config.read(file_name)
+        try:
+            details = config['AWS_MYSQL_CONFIG']
+            host = details['host']
+            user = details['user']
+            password = details['password']
+            database = details['database']
+            port = int(details['port'])
+            return cls(host, user, password, database, port)
+        except configparser.NoOptionError as err:
+            cls.logger.exception("configparser.NoOptionError:")
+            cls.logger.exception(err)
+            return None
+
+    def test_can_reach_host(self):
+        rds_host = self.host
+        port = self.port
+        try:
+            with socket.create_connection((rds_host, port), timeout=5):
+                pass
+            self.logger.info(f"Can reach AWS RDS host: {rds_host}")
+        except Exception as e:
+            self.logger.exception(
+                f"Failed to reach AWS RDS host: {rds_host}, Error: {e}")
+
+    def connect_to_database(self):
+        try:
+            self.connection = pymysql.connect(
+                host=self.host,
+                user=self.user,
+                password=self.password,
+                database=self.database,
+                port=self.port
+            )
+            self.logger.debug("Connected to the database.")
+        except Exception as e:
+            self.logger.debug(f"Error connecting to the database: {e}")
+
+    def get_database_info(self):
+        try:
+            with self.connection.cursor() as cursor:
+                cursor.execute("SHOW DATABASES")
+                result = cursor.fetchall()
+                databases = [item[0] for item in result]
+                print("List of databases:")
+                for db in databases:
+                    self.logger.info(f"Name: {db}")
+        except Exception as e:
+            self.logger.exception(f"Error fetching database information: {e}")
+
+    def get_database_properties(self, database_name):
+        """Retrieve properties of a database."""
+        db_info = {}
+        try:
+            with self.connection.cursor() as cursor:
+                cursor.execute(f"SHOW CREATE DATABASE `{database_name}`")
+                result = cursor.fetchone()
+                if result:
+                    # Updated line for debugging
+                    self.logger.debug("Result: %s", result)
+                    # Parse the result to extract database properties
+                    create_statement = result[1]
+                    owner_start = create_statement.find(
+                        "CREATE DATABASE") + len("CREATE DATABASE")
+                    owner_end = create_statement.find("CHARACTER SET") if create_statement.find(
+                        "CHARACTER SET") != -1 else len(create_statement)
+                    owner = create_statement[owner_start:owner_end].strip()
+                    collation_start = create_statement.find("COLLATE") + len(
+                        "COLLATE") if create_statement.find("COLLATE") != -1 else len(create_statement)
+                    collation_end = len(create_statement)
+                    collation = create_statement[collation_start:collation_end].strip(
+                    )
+                    db_info['Owner'] = owner
+                    db_info['Collation'] = collation
+        except Exception as e:
+            self.logger.exception("Error fetching database properties: %s", e)
+        return db_info
+
+    def check_database_exists(self, database_name):
+        """Check if a database exists."""
+        try:
+            with self.connection.cursor() as cursor:
+                cursor.execute(f"SHOW DATABASES LIKE '{database_name}'")
+                result = cursor.fetchone()
+                return result is not None
+        except Exception as e:
+            self.logger.exception("Error checking if database exists: %s", e)
+            return False
+
+    def create_database(self, database_name):
+        """Create a database."""
+        try:
+            with self.connection.cursor() as cursor:
+                cursor.execute(f"CREATE DATABASE `{database_name}`")
+            self.logger.info(
+                f"Database '{database_name}' created successfully.")
+        except Exception as e:
+            self.logger.exception(
+                f"Error creating database '{database_name}': {e}")
+
+    def remove_database(self, database_name):
+        """Remove a database."""
+        try:
+            with self.connection.cursor() as cursor:
+                cursor.execute(f"DROP DATABASE `{database_name}`")
+            self.logger.info(
+                f"Database '{database_name}' removed successfully.")
+        except Exception as e:
+            self.logger.exception(
+                f"Error removing database '{database_name}': {e}")
+
+    def close_connection(self):
+        if self.connection and self.connection.open:
+            self.connection.close()
+            self.logger.debug("Connection closed.")
+
+
+if __name__ == "__main__":
+    print("Executing awsmysqllib.py __name__")
+    aws_database = AWSMySQLLib.init_from_file('awsmysql.cfg')
