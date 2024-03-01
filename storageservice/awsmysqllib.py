9602e3c1e9667a1035520fcda0cc3e5fbe80cfbc
dmickelson <davidpmickelson@gmail.com>
2024-03-01 11:48:26 -0500

    added table and inserting

--- storageservice/awsmysqllib.py
+++ storageservice/awsmysqllib.py
@@ -40,16 +40,46 @@ class AWSMySQLLib:
     def test_can_reach_host(self) -> bool:
         rds_host = self.host
         port = self.port
+        self.logger.debug(
+            f"Attempting to reach AWS RDS host: {rds_host}:{port}")
         try:
             with socket.create_connection((rds_host, port), timeout=5):
-                return True
-            self.logger.info(f"Can reach AWS RDS host: {rds_host}")
+                pass
+            self.logger.info(f"Can reach AWS RDS host: {rds_host}:{port}")
+            return True
         except Exception as e:
             self.logger.exception(
-                f"Failed to reach AWS RDS host: {rds_host}, Error: {e}")
+                f"Failed to reach AWS RDS host: {rds_host}:{port}, Error: {e}")
+            return False
+
+    def connect_to_rds_host(self) -> bool:
+        rds_host = self.host
+        port = self.port
+        self.logger.debug(
+            f"Attempting to reach AWS RDS host: {rds_host}:{port}")
+        try:
+            self.connection = pymysql.connect(
+                host=self.host,
+                user=self.user,
+                password=self.password,
+                port=self.port
+            )
+            self.logger.debug(f"Connected to AWS RDS host: {rds_host}:{port}")
+            version_info = self.connection.get_server_info()
+            self.logger.debug(
+                f"Connected to AWS MySQL RDS (version {version_info})")
+            return True
+        except Exception as e:
+            self.logger.exception(
+                f"Error connecting to AWS RDS host: {rds_host}:{port}, Error: {e}")
             return False
 
     def connect_to_database(self) -> bool:
+        rds_host = self.host
+        database = self.database
+        port = self.port
+        self.logger.debug(
+            f"Attempting to reach database: {database}")
         try:
             self.connection = pymysql.connect(
                 host=self.host,
@@ -58,22 +88,28 @@ class AWSMySQLLib:
                 database=self.database,
                 port=self.port
             )
-            self.logger.debug("Connected to the database.")
+            self.logger.debug(f"Connected to the database: {database}")
+            version_info = self.connection.get_server_info()
+            self.logger.debug(
+                f"Connected to AWS MySQL RDS (version {version_info})")
             return True
         except Exception as e:
-            self.logger.debug(f"Error connecting to the database: {e}")
+            self.logger.exception(f"Error connecting to the database: {e}")
             return False
 
     def get_database_info(self) -> list:
+        """Retrieve detailed information about databases."""
+        databases_info = []
         try:
             with self.connection.cursor() as cursor:
                 cursor.execute("SHOW DATABASES")
                 result = cursor.fetchall()
-                databases = [item[0] for item in result]
-                self.logger.info("List of databases:")
-                for db in databases:
-                    self.logger.info(f"Name: {db}")
-                return databases
+                self.logger.debug("List of databases:")
+                for db_name in [item[0] for item in result]:
+                    self.logger.debug(f"Name: {db_name}")
+                    db_info = self.get_database_properties(db_name)
+                    databases_info.append(db_info)
+                return databases_info
         except Exception as e:
             self.logger.exception(f"Error fetching database information: {e}")
             return None
@@ -113,6 +149,10 @@ class AWSMySQLLib:
                 cursor.execute(f"SHOW DATABASES LIKE '{database_name}'")
                 result = cursor.fetchone()
                 return result is not None
+        except AttributeError as e:
+            self.logger.exception(
+                "No Connection in checking if database exists: %s", e)
+            return False
         except Exception as e:
             self.logger.exception("Error checking if database exists: %s", e)
             return False
@@ -141,6 +181,99 @@ class AWSMySQLLib:
         except Exception as e:
             self.logger.exception(
                 f"Error removing database '{database_name}': {e}")
+            return False
+
+    def create_table(self, table_name: str, columns: Dict[str, str]) -> bool:
+        """
+        Create a table with the specified name and columns.
+
+        Args:
+            table_name (str): The name of the table.
+            columns (Dict[str, str]): A dictionary specifying column names and their data types.
+
+        Returns:
+            bool: True if the table creation is successful, False otherwise.
+        """
+        try:
+            with self.connection.cursor() as cursor:
+                column_definitions = ', '.join(
+                    f"{name} {data_type}" for name, data_type in columns.items())
+                query = f"CREATE TABLE {table_name} ({column_definitions})"
+                cursor.execute(query)
+            self.connection.commit()
+            self.logger.info(f"Table '{table_name}' created successfully.")
+            return True
+        except Exception as e:
+            self.logger.exception(f"Error creating table '{table_name}': {e}")
+            return False
+
+    def list_tables_with_columns(self) -> List[Dict[str, Union[str, List[Dict[str, str]]]]]:
+        """
+        List all tables within the database with details about their columns.
+
+        Returns:
+            List[Dict[str, Union[str, List[Dict[str, str]]]]]: A list of dictionaries containing table information.
+        """
+        table_info_list = []
+
+        try:
+            with self.connection.cursor() as cursor:
+                cursor.execute("SHOW TABLES")
+                tables = [table[0] for table in cursor.fetchall()]
+
+                for table_name in tables:
+                    table_info = {'table_name': table_name, 'columns': []}
+
+                    # Retrieve column information for the current table
+                    cursor.execute(f"DESCRIBE {table_name}")
+                    columns_info = cursor.fetchall()
+
+                    for column_info in columns_info:
+                        column_details = {
+                            'column_name': column_info[0],
+                            'data_type': column_info[1],
+                            'nullable': 'YES' if column_info[2] == 'YES' else 'NO',
+                            'key': column_info[3],
+                            'default': column_info[4],
+                            'extra': column_info[5]
+                        }
+
+                        table_info['columns'].append(column_details)
+
+                    table_info_list.append(table_info)
+
+            self.logger.info("List of tables with column details:")
+            for table_info in table_info_list:
+                self.logger.info(f"Table: {table_info['table_name']}")
+                for column_info in table_info['columns']:
+                    self.logger.info(f"  Column: {column_info}")
+
+            return table_info_list
+
+        except Exception as e:
+            self.logger.exception(
+                f"Error listing tables with column details: {e}")
+            return []
+
+    def delete_table(self, table_name: str) -> bool:
+        """
+        Delete a table with the specified name.
+
+        Args:
+            table_name (str): The name of the table to delete.
+
+        Returns:
+            bool: True if the table deletion is successful, False otherwise.
+        """
+        try:
+            with self.connection.cursor() as cursor:
+                query = f"DROP TABLE IF EXISTS {table_name}"
+                cursor.execute(query)
+            self.connection.commit()
+            self.logger.info(f"Table '{table_name}' deleted successfully.")
+            return True
+        except Exception as e:
+            self.logger.exception(f"Error deleting table '{table_name}': {e}")
             return False
 
     def insert_record(self, table: str, data: Dict[str, Union[str, int, float]]) -> int:
--- tests/test_aws_db_connection.py
+++ tests/test_aws_db_connection.py
@@ -1,19 +1,11 @@
 import pytest
 from storageservice import awsmysqllib
-from typing import Dict, Union
 
-# Set your actual AWS RDS credentials
-AWS_RDS_HOST = 'your_rds_host'
-AWS_RDS_USER = 'your_rds_user'
-AWS_RDS_PASSWORD = 'your_rds_password'
-AWS_RDS_DATABASE = 'your_database'
-AWS_RDS_PORT = 3306  # Change to your actual port
+# ? pytest -sv test_aws_db_connection.py
 
 
-# ? Set your actual AWS RDS credentials
-AWS_RDS_CONFIG_FILE = 'awsmysql.cfg'  # Change to your actual file path
-
-# ? pytest -v test_aws_db_connection.py
+TABLE_NAME = 'example_table'
+AWS_RDS_CONFIG_FILE = './awsmysql.cfg'  # Change to your actual file path
 
 
 @pytest.fixture
@@ -24,61 +16,118 @@ def aws_database():
     aws_db.close_connection()
 
 
-def test_can_reach_host(aws_database):
-    assert aws_database.test_can_reach_host(), "Failed to reach AWS RDS host"
+@pytest.fixture
+def aws_rds_host():
+    aws_db = awsmysqllib.AWSMySQLLib.init_from_file(AWS_RDS_CONFIG_FILE)
+    aws_db.connect_to_rds_host()
+    yield aws_db
+    aws_db.close_connection()
 
 
-def test_database_connection(aws_database):
-    assert aws_database.connect_to_database(), "Connection to AWS MySQL RDS failed"
+# ? pytest -sv test_aws_db_connection.py::test_can_reach_host
+def test_can_reach_host(aws_rds_host):
+    assert aws_rds_host.test_can_reach_host(), "Failed to reach AWS RDS host"
 
-    version_info = aws_database.connection.get_server_info()
+
+# ? pytest -sv test_aws_db_connection.py::test_rds_host_connection
+def test_rds_host_connection(aws_rds_host):
+    assert aws_rds_host.test_can_reach_host(
+    ), "Failed to reach AWS RDS host"
+
+    version_info = aws_rds_host.connection.get_server_info()
     print(f"Connected to AWS MySQL RDS (version {version_info})")
 
-    # Retrieve and print detailed information about databases
-    databases_info = aws_database.get_database_info()
+    # * Retrieve and print detailed information about databases
+    databases_info = aws_rds_host.get_database_info()
     print("List of databases:")
     for db_info in databases_info:
         print(
-            f"Name: {db_info.get('Owner', 'N/A')}, Owner: {db_info.get('Collation', 'N/A')}")
+            f"Name: {db_info.get('Owner')}, Owner: {db_info.get('Collation')}")
 
-    # Check if 'your_database' exists
-    print("Checking if 'your_database' exists:")
-    exists = aws_database.check_database_exists('your_database')
-    print(f"Checking If 'your_database' database exists: {exists}")
+
+def test_create_database(aws_rds_host):
+    # * Check if 'your_database' exists
+    your_database = aws_rds_host.database
+    print(f"Initial Checking if '{your_database}' exists:")
+    exists = aws_rds_host.check_database_exists(your_database)
+    print(f"Checking If '{your_database}' database exists: {exists}")
 
     if not exists:
-        print("'your_database' does not exist, attempting to create it...")
-        assert aws_database.create_database(
-            'your_database'), "Failed to create 'your_database', test aborted."
+        print(f"'{your_database}' does not exist, attempting to create it...")
+        assert aws_rds_host.create_database(
+            your_database), f"Failed to create '{your_database}', test aborted."
         print("Updated List of databases:")
+        databases_info = aws_rds_host.get_database_info()
         for db_info in databases_info:
             print(
-                f"Name: {db_info.get('Owner', 'N/A')}, Owner: {db_info.get('Collation', 'N/A')}")
+                f"Name: {db_info.get('Owner')}, Owner: {db_info.get('Collation')}")
+        exists = aws_rds_host.check_database_exists(your_database)
+        print(f"Checking If '{your_database}' database exists: {exists}")
+    else:
+        print(f"The '{your_database}' database exists: {exists}")
+        assert False
 
-    print(f"The 'your_database' database exists: {exists}")
 
-    # Additional test logic can be added here...
+def test_can_reach_database(aws_database):
+    assert aws_database.connect_to_database(
+    ), "Connection to AWS MySQL RDS Database failed"
 
-    # Remove 'your_database' if it was created during the test
-    if exists:
-        print("Removing 'your_database'...")
-        assert aws_database.remove_database(
-            'your_database'), "Failed to remove 'your_database'"
+
+def test_create_table(aws_database):
+    columns = {'id': 'INT', 'name': 'VARCHAR(255)', 'age': 'INT'}
+    assert aws_database.create_table(
+        TABLE_NAME, columns), f"Table Creation failed {TABLE_NAME}"
+
+
+def test_list_tables(aws_database):
+    result = aws_database.list_tables_with_columns()
+    print("List of Tables with Columns:")
+    print(result)
+    assert result is not None, "Failed to list tables with columns"
 
 
 def test_insert_update_delete_record(aws_database):
-    table_name = 'your_table'  # Replace with your actual table name
+    table_name = TABLE_NAME
 
-    # Insert a record
-    data_to_insert = {'column1': 'value1', 'column2': 'value2'}
+    # * Insert a record
+    # * columns = {'id': 'INT', 'name': 'VARCHAR(255)', 'age': 'INT'}
+    data_to_insert = {'id': 123, 'name': 'John', 'age': 40}
     record_id = aws_database.insert_record(table_name, data_to_insert)
     assert record_id != -1, "Failed to insert record"
 
-    # Update the inserted record
-    data_to_update = {'column1': 'new_value1', 'column2': 'new_value2'}
+    # * Update the inserted record
+    data_to_update = {'age': '20'}
     assert aws_database.update_record(
         table_name, record_id, data_to_update), "Failed to update record"
 
-    # Delete the updated record
+    # * Delete the updated record
     assert aws_database.delete_record(
         table_name, record_id), "Failed to delete record"
+
+
+def test_delete_table(aws_database):
+    assert aws_database.delete_table(
+        TABLE_NAME), f"Table Delete failed {TABLE_NAME}"
+
+
+def test_delete_database(aws_database):
+    your_database = aws_database.database
+    print(f"Initial Checking if '{your_database}' exists:")
+    exists = aws_database.check_database_exists(your_database)
+    print(f"Checking If '{your_database}' database exists: {exists}")
+    if exists:
+        print(f"The '{your_database}' database exists: {exists}")
+        print(f"Removing '{your_database}'...")
+        assert aws_database.remove_database(
+            your_database), f"Failed to remove '{your_database}'"
+
+        print("Updated List of databases:")
+        databases_info = aws_database.get_database_info()
+        for db_info in databases_info:
+            print(
+                f"Name: {db_info.get('Owner')}, Owner: {db_info.get('Collation')}")
+        exists = aws_database.check_database_exists(your_database)
+        print(f"Checking If '{your_database}' database exists: {exists}")
+    else:
+        print(f"The '{your_database}' database DOES NOT exists: {exists}")
+        assert False
