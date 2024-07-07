09ddad1f57b79e51ad8191eb08265b165a8fdd36
dmickelson <davidpmickelson@gmail.com>
2024-07-06 21:37:42 -0400

    updated tests docstring and readme

--- tests/test_aws_db_connection.py
+++ tests/test_aws_db_connection.py
@@ -10,6 +10,12 @@ AWS_RDS_CONFIG_FILE = './awsmysql.cfg'  # Change to your actual file path
 
 @pytest.fixture
 def aws_database():
+    """
+    Fixture to set up an AWSMySQLLib instance connected to a specific database.
+
+    Yields:
+        AWSMySQLLib: An instance of AWSMySQLLib connected to the database.
+    """
     aws_db = awsmysqllib.AWSMySQLLib.init_from_file(AWS_RDS_CONFIG_FILE)
     aws_db.connect_to_database()
     yield aws_db
@@ -18,6 +24,12 @@ def aws_database():
 
 @pytest.fixture
 def aws_rds_host():
+    """
+    Fixture to set up an AWSMySQLLib instance connected to an RDS host.
+
+    Yields:
+        AWSMySQLLib: An instance of AWSMySQLLib connected to the RDS host.
+    """
     aws_db = awsmysqllib.AWSMySQLLib.init_from_file(AWS_RDS_CONFIG_FILE)
     aws_db.connect_to_rds_host()
     yield aws_db
@@ -26,11 +38,23 @@ def aws_rds_host():
 
 # ? pytest -sv test_aws_db_connection.py::test_can_reach_host
 def test_can_reach_host(aws_rds_host):
+    """
+    Test to check if the AWS RDS host can be reached.
+
+    Args:
+        aws_rds_host (AWSMySQLLib): Fixture instance connected to the RDS host.
+    """
     assert aws_rds_host.test_can_reach_host(), "Failed to reach AWS RDS host"
 
 
 # ? pytest -sv test_aws_db_connection.py::test_rds_host_connection
 def test_rds_host_connection(aws_rds_host):
+    """
+    Test to check connection to AWS RDS host and retrieve database information.
+
+    Args:
+        aws_rds_host (AWSMySQLLib): Fixture instance connected to the RDS host.
+    """
     assert aws_rds_host.test_can_reach_host(
     ), "Failed to reach AWS RDS host"
 
@@ -46,7 +70,12 @@ def test_rds_host_connection(aws_rds_host):
 
 
 def test_create_database(aws_rds_host):
-    # * Check if 'your_database' exists
+    """
+    Test to check if a database can be created if it doesn't already exist.
+
+    Args:
+        aws_rds_host (AWSMySQLLib): Fixture instance connected to the RDS host.
+    """
     your_database = aws_rds_host.database
     print(f"Initial Checking if '{your_database}' exists:")
     exists = aws_rds_host.check_database_exists(your_database)
@@ -69,17 +98,35 @@ def test_create_database(aws_rds_host):
 
 
 def test_can_reach_database(aws_database):
+    """
+    Test to check if the AWS MySQL RDS database can be reached.
+
+    Args:
+        aws_database (AWSMySQLLib): Fixture instance connected to the database.
+    """
     assert aws_database.connect_to_database(
     ), "Connection to AWS MySQL RDS Database failed"
 
 
 def test_create_table(aws_database):
+    """
+    Test to check if a table can be created in the database.
+
+    Args:
+        aws_database (AWSMySQLLib): Fixture instance connected to the database.
+    """
     columns = {'id': 'INT', 'name': 'VARCHAR(255)', 'age': 'INT'}
     assert aws_database.create_table(
         TABLE_NAME, columns), f"Table Creation failed {TABLE_NAME}"
 
 
 def test_list_tables(aws_database):
+    """
+    Test to list all tables with columns in the database.
+
+    Args:
+        aws_database (AWSMySQLLib): Fixture instance connected to the database.
+    """
     result = aws_database.list_tables_with_columns()
     print("List of Tables with Columns:")
     print(result)
@@ -87,6 +134,12 @@ def test_list_tables(aws_database):
 
 
 def print_entries(entries):
+    """
+    Helper function to print entries from a table.
+
+    Args:
+        entries (list): List of entries to print.
+    """
     # * Check if the method returned entries
     print(f"Printing Entries")
     if entries is not None:
@@ -97,6 +150,12 @@ def print_entries(entries):
 
 
 def test_insert_update_delete_record(aws_database):
+    """
+    Test to insert, update, and delete a record in the database.
+
+    Args:
+        aws_database (AWSMySQLLib): Fixture instance connected to the database.
+    """
     table_name = TABLE_NAME
     # * Insert a record
     # * columns = {'id': 'INT', 'name': 'VARCHAR(255)', 'age': 'INT'}
@@ -122,11 +181,23 @@ def test_insert_update_delete_record(aws_database):
 
 
 def test_delete_table(aws_database):
+    """
+    Test to delete a table from the database.
+
+    Args:
+        aws_database (AWSMySQLLib): Fixture instance connected to the database.
+    """
     assert aws_database.delete_table(
         TABLE_NAME), f"Table Delete failed {TABLE_NAME}"
 
 
 def test_delete_database(aws_database):
+    """
+    Test to delete a database if it exists.
+
+    Args:
+        aws_database (AWSMySQLLib): Fixture instance connected to the database.
+    """
     your_database = aws_database.database
     print(f"Initial Checking if '{your_database}' exists:")
     exists = aws_database.check_database_exists(your_database)
